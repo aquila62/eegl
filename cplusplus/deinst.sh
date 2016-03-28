baf38d1b896b9ab8db73e69d41bc7230c713e6e6
aquila62 <orvalbounds@yahoo.com>
2016-03-27 21:48:45 -0500

    Added a chi square test

--- cplusplus/deinst.sh
+++ cplusplus/deinst.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 make -f tsteegl.mak clean
+make -f eeglchi.mak clean
 make -f eeglraw.mak clean
 make -f eeglgen.mak clean
 make -f ranwlk.mak clean
--- cplusplus/inst.sh
+++ cplusplus/inst.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 make -f tsteegl.mak
+make -f eeglchi.mak
 make -f eeglraw.mak
 make -f eeglgen.mak
 make -f ranwlk.mak
