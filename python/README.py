3214925374aacfd315ab40fb3d6ac56e6131adc8
aquila62 <orvalbounds@yahoo.com>
2016-03-27 20:54:41 -0500

    Corrected the performance estimate

--- python/README.py
+++ python/README.py
@@ -4,7 +4,7 @@ Follow the examples in eeglgen.py and eeglraw.py for
 creating your own programs using the eegl generator.
 
 eeglraw.py | dieharder -g 200 -p 1 -d 0
-is 100 times slower than C++ eegl and 1000 times slower than C eegl.
+is 8 times slower than C++ eegl and 100 times slower than C eegl.
 
 To read more about the eegl random number generator, read
 README.eegl in the C repository of eegl.
