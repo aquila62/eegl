a96285a72b0ef173ec30bbe123648508bf74fc57
aquila62 <orvalbounds@yahoo.com>
2016-04-02 05:24:04 -0500

    Added comments and changed last line of report
    Changed last line of report from 90% to 95% based on GSL pdf function for chi square

--- python/eeglchi.py
+++ python/eeglchi.py
@@ -10,9 +10,11 @@ diffsq   = 0.0
 expected = 1024.0
 for i in range(16):
 	tally.append(0.0)
+# 16384 4-bit samples
 for i in range(16384):
 	indx = eegl.eeglpwr(4)
 	tally[indx] = tally[indx] + 1.0
+# calculate chi square
 for i in range(16):
 	diff = expected - tally[i]
 	diffsq = diff * diff
@@ -20,5 +22,4 @@ for i in range(16):
 print 'Total samples 16384'
 print 'Chi square', chisq
 print '15 degrees of freedom'
-print 'Chi square at  5% is  7.261'
-print 'Chi square at 95% is 24.996'
+print '95% of the time, the chi square is between 6.262 and 27.488'
